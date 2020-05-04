240dd2cc868b2e668a9f5a7abb777a29bd229f96
Duncanwar <semunda9@gmail.com>
2020-05-04 20:53:01 +0200

    component part2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Menu from './components/MenuComponents'
+import {DISHES} from './shared/dishes'
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+super(props);
+this.state={
+dishes: DISHES
+};
+  }
+render(){
   return (
     <div className="App">
-      <Menu />
+      <Menu  dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
